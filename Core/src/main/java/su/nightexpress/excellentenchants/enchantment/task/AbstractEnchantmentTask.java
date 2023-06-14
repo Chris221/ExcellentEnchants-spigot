ea4f08b6df64827ddf5ebcf425a6f71cba347a9b
Christopher Siena <zenatron@gmail.com>
2023-06-13 21:40:33 -0400

    building locally

--- Core/src/main/java/su/nightexpress/excellentenchants/enchantment/task/AbstractEnchantmentTask.java
+++ Core/src/main/java/su/nightexpress/excellentenchants/enchantment/task/AbstractEnchantmentTask.java
@@ -1,7 +1,7 @@
 package su.nightexpress.excellentenchants.enchantment.task;
 
 import org.bukkit.entity.LivingEntity;
-import org.jetbrains.annotations.NotNull;
+import su.nightexpress.excellentenchants.enchantment.task.NonNull;
 import su.nexmedia.engine.api.server.AbstractTask;
 import su.nightexpress.excellentenchants.ExcellentEnchants;
 import su.nightexpress.excellentenchants.config.Config;
@@ -12,12 +12,12 @@ import java.util.Set;
 
 public abstract class AbstractEnchantmentTask extends AbstractTask<ExcellentEnchants> {
 
-    public AbstractEnchantmentTask(@NotNull ExcellentEnchants plugin, long interval, boolean async) {
+    public AbstractEnchantmentTask(@NonNull ExcellentEnchants plugin, long interval, boolean async) {
         super(plugin, interval, async);
     }
 
-    @NotNull
-    protected Collection<@NotNull ? extends LivingEntity> getEntities() {
+    @NonNull
+    protected Collection<@NonNull ? extends LivingEntity> getEntities() {
         Set<LivingEntity> list = new HashSet<>(plugin.getServer().getOnlinePlayers());
 
         if (Config.ENCHANTMENTS_ENTITY_PASSIVE_FOR_MOBS.get()) {
---
+++ Core/src/main/java/su/nightexpress/excellentenchants/enchantment/task/NonNull.java
@@ -0,0 +1,16 @@
+package su.nightexpress.excellentenchants.enchantment.task;
+
+import java.lang.annotation.*;
+
+/**
+ * you have to define your own NonNull annotation to be able to mark a generic as not null
+ * because IDEA does not allow using it's @NonNull for java 7 compatibility reasons
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target(value=ElementType.TYPE_USE) // this is the parameter that was introduced in java 8
+public @interface NonNull {
+    String value() default "";
+
+    Class<? extends Exception> exception() default Exception.class;
+}
